f563c4b6b178d95cdbe04304dfab8c538b44dcd3
jakub <>
2021-04-21 18:20:15 +0200

    made oop change css add searching by input

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 'use strict';
 const seriesContainer = document.querySelector('.series');
-
-/* <div class="anime">
-<img class="anime__img" src="${data.image_url}" />
-<div class="anime__data">
-  <h3 class="anime__name">${data.title}</h3>
-  <h4 class="anime__score">score: ${data.score}</h4>
-  <p class="anime__row"><span> type: </span>${data.type}</p>
-  <p class="anime__row"><span>episodes: </span>${data.episodes}</p>
-</div>
-</div> */
-
-const renderAnime = data => {
-  const html = `
-  <div class="anime">
-    <div class="anime__data">
-      <img class="anime__img" src="${data.image_url}" />
-      <h3 class="anime__name">${data.title}</h3>
-      <h4 class="anime__score">score: ${data.score}</h4>
-      <p class="anime__row "><span> type: </span>${data.type}</p>
-      <p class="anime__row "><span>episodes: </span>${data.episodes}</p>
-    </div>
-  </div>
-  `;
-
-  seriesContainer.insertAdjacentHTML('beforeend', html);
-  // html.style.backgroundImage = `url(${data.image_url})`;
-  console.log();
-};
-
-const searchAnime = async function (animeName) {
-  try {
-    const anime = await fetch(
-      `https://api.jikan.moe/v3/search/anime?q=${animeName}`
-    );
-    const animeData = await anime.json();
-    const animeResults = animeData.results;
-
-    animeResults.forEach(e => {
-      renderAnime(e);
-    });
-  } catch (err) {
-    console.error(err);
+const searchInput = document.querySelector('.inputAnime');
+const searchButton = document.querySelector('.btn');
+class Anime {
+  constructor(name) {
+    this.name = name;
   }
-};
 
-searchAnime('naruto');
+  async searchAnime() {
+    try {
+      const anime = await fetch(
+        `https://api.jikan.moe/v3/search/anime?q=${this.name}`
+      );
+      const animeData = await anime.json();
+      const animeResults = animeData.results;
+
+      animeResults.forEach(e => {
+        this.renderAnime(e);
+      });
+
+      seriesContainer.style.opacity = 1;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  renderAnime(data) {
+    const html = `
+    <div class="anime">
+      <div class="anime__data">
+        <img class="anime__img" src="${data.image_url}" />
+        <h3 class="anime__name">${data.title}</h3>
+        <h4 class="anime__score">score: ${data.score}</h4>
+        <p class="anime__row "><span> type: </span>${data.type}</p>
+        <p class="anime__row "><span>episodes: </span>${data.episodes}</p>
+      </div>
+    </div>
+    `;
+
+    seriesContainer.insertAdjacentHTML('beforeend', html);
+  }
+}
+
+searchButton.addEventListener('click', e => {
+  e.preventDefault();
+  const a = new Anime(searchInput.value);
+  a.searchAnime();
+  searchInput.value = '';
+});
